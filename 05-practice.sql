d4919f693bdb92e34bf4bc3b9636b9246c2c2dc7
Sasaki-Momoka <momosasa39@gmail.com>
2026-06-23 13:12:03 +0900

    fix3

--- 05-practice.sql
+++ 05-practice.sql
@@ -3,89 +3,86 @@
 SELECT
     name
 FROM
-    cities;
+    countries;
 
 -- 問2
 -- ヨーロッパに属する国をすべて抽出してください。
 SELECT
-   name
+   *
 FROM
-    country
+    countries
 WHERE
     continent = 'Europe';
 
 -- 問3
 -- ヨーロッパ以外に属する国をすべて抽出してください。
-SELECT name
- FROM country
+SELECT *
+ FROM countries
 WHERE continent <> 'Europe';
 
 -- 問4
 -- 人口が10万人以上の国をすべて抽出してください。
-SELECT name
- FROM country
+SELECT *
+ FROM countries
 WHERE population >= 100000;
 
 -- 問5
 -- 平均寿命が56歳から76歳の国をすべて抽出してください。
-SELECT age
-FROM celebrities
-WHERE < (
-    SELECT AVG(age) FROM celebrities
-)
+SELECT *
+FROM countries
+WHERE life_expectancy BETWEEN 56 AND 76;
 
 -- 問6
 -- 国コードがNLB,ALB,DZAのもの市区町村をすべて抽出してください。
-SELECT name
+SELECT *
 FROM cities
 WHERE country_code IN ('NLB', 'ALB', 'DZA');
 
 -- 問7
 -- 独立独立記念日がない国をすべて抽出してください。
-SELECT name FROM country WHERE independence_date IS NULL;
+SELECT * FROM countries WHERE indep_year IS NULL;
 
 
 -- 問8
 -- 独立独立記念日がある国をすべて抽出してください。
-SELECT name FROM country WHERE independence_date IS NOT NULL;
+SELECT * FROM countries WHERE indep_year IS NOT NULL;
 
 
 -- 問9
 -- 名前の末尾が「ia」で終わる国を抽出してください。
-SELECT name FROM country WHERE RIGHT(name, 2) = 'ia';
+SELECT name FROM countries WHERE RIGHT(name, 2) = 'ia';
 
 -- 問10
 -- 名前の中に「st」が含まれる国を抽出してください。
-
+SELECT * FROM countries WHERE name ILIKE '%st%';
 
 -- 問11
 -- 名前が「an」で始まる国を抽出してください。
-
+SELECT name FROM countries WHERE LEFT(name, 2) = 'An';
 
 -- 問12
 -- 全国の中から独立記念日が1990年より前または人口が10万人より多い国を全て抽出してください。
-
-
+--SELECT * FROM countries WHERE indep_year IS NOT NULL AND
+--(indep_year < 1990 OR population > 100000);
+SELECT * FROM countries WHERE indep_year < 1990 OR population > 100000;
 -- 問13
 -- コードがDZAもしくはALBかつ独立記念日が1990年より前の国を全て抽出してください。
-
-
+SELECT * FROM countries WHERE (code = 'DZA' OR code = 'ALB') AND indep_year < 1990;
 -- 問14
 -- 全ての地方をグループ化せずに表示してください。
-
 
 -- 問15
 -- 国名と人口を以下のように表示させてください。シングルクォートに注意してください。
 -- 「Arubaの人口は103000人です」
-
-
+SELECT name || 'の人口は' || population || '人です' AS POULATION
+FROM countries;
 -- 問16
 -- 平均寿命が短い順に国名を表示させてください。ただしNULLは表示させないでください。
-
+SELECT name, life_expectancy FROM countries WHERE life_expectancy IS NOT NULL ORDER BY life_expectancy ASC;
 
 -- 問17
 -- 平均寿命が長い順に国名を表示させてください。ただしNULLは表示させないでください。
-
+SELECT name, life_expectancy FROM countries WHERE life_expectancy IS NOT NULL ORDER BY life_expectancy DESC;
 
 -- 問18
 -- 平均寿命が長い順、独立記念日が新しい順に国を表示させてください。
